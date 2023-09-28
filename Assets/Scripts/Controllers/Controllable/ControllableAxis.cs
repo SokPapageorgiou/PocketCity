97422095b7e0e8482610d277b200c8fb04957ee6
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2023-09-28 10:45:27 +0200

    Add ControllableAxis class to avoid duplicated code

---
+++ Assets/Scripts/Controllers/Controllable/ControllableAxis.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Controllers.Controllable
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public abstract class ControllableAxis : MonoBehaviour, IControllableAxis
+    {
+        protected NavMeshAgent Agent;
+        protected Vector3 TargetDirection;
+
+        private void Awake()
+        {
+            Agent = GetComponent<NavMeshAgent>();
+            TargetDirection = Vector3.zero;
+        }
+
+        public void Control(bool autopilot, float horizontal, float vertical)
+        {
+            SetTargetDirection(horizontal, vertical);
+            
+            if (autopilot)
+            {
+                RunAutopilot();
+            }
+            else
+            {
+                RunManual();
+            }
+        }
+        
+        private void RunAutopilot()
+        {
+            Agent.Move(TargetDirection);
+        }
+
+        private void SetTargetDirection(float horizontal, float vertical)
+        {
+            TargetDirection.x = horizontal;
+            TargetDirection.z = vertical;
+            TargetDirection.Normalize();
+        }
+
+        protected abstract void RunManual();
+    }
+}
--- Assets/Scripts/Controllers/Controllable/Humanoid.cs
+++ Assets/Scripts/Controllers/Controllable/Humanoid.cs
@@ -1,32 +1,10 @@
-using UnityEngine;
-using UnityEngine.AI;
-using Vector3 = UnityEngine.Vector3;
-
 namespace Controllers.Controllable
 {
-    [RequireComponent(typeof(NavMeshAgent))]
-    public class Humanoid : MonoBehaviour, IControllableAxis
-    {
-        private NavMeshAgent _agent;
-        private Vector3 _targetDirection;
-
-        private void Awake()
+    public class Humanoid : ControllableAxis
+    { 
+        protected override void RunManual()
         {
-            _agent = GetComponent<NavMeshAgent>();
-            _targetDirection = Vector3.zero;
-        }
-
-        public void Control(bool autopilot, float horizontal, float vertical)
-        {
-            SetTargetDirection(horizontal, vertical);
-            _agent.velocity = _targetDirection * _agent.speed;
-        }
-
-        private void SetTargetDirection(float horizontal, float vertical)
-        {
-            _targetDirection.x = horizontal;
-            _targetDirection.z = vertical;
-            _targetDirection.Normalize();
+            Agent.velocity = TargetDirection * Agent.speed;
         }
     }
 }
