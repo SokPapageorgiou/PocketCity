2446c76a3198d9079f445e7cc4bd31951fd4318a
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2023-10-04 12:09:31 +0200

    Add SceneState singleton

---
+++ Assets/Scripts/Commons/SceneState.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using ObjPool;
+using UnityEngine;
+
+namespace Commons
+{
+    public class SceneState : MBSingleton<SceneState>
+    {
+        [SerializeField] private List<PoolLibrary> poolLibraries;
+        
+        public Pool Pool { get; private set; }
+        
+        protected override void LoadStates()
+        {
+            Pool = new PoolFactory().Create(poolLibraries);
+        }
+    }
+}
