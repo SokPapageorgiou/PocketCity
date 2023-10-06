78a5d3dedaaf3688dce73fe338565da3dc336c00
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2023-10-06 10:44:53 +0200

    Add Humanoids move to Randon position

--- Assets/Scripts/Controllers/AI/HumanoidsAI.cs
+++ Assets/Scripts/Controllers/AI/HumanoidsAI.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Commons;
 using Controllers.Controllable;
+using ObjPool;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -7,29 +11,58 @@ namespace Controllers.AI
 {
     public class HumanoidsAI : MonoBehaviour
     {
+        private const PoolTypes PoolType = PoolTypes.Humanoid;
         private const bool AutoPilot = true;
 
-        [SerializeField]
-        private List<Humanoid> humanoids;
-
         private readonly Dictionary<Humanoid, Vector3> _humanoidsPositions = new ();
+        
+        private Pool _pool;
+        
+        [SerializeField]
+        private uint spawnRateSec = 1;
+        [SerializeField]
+        private float distanceThreshold = 1.5f;
 
-        private void Awake()
+        private void Start()
         {
-            foreach (var humanoid in humanoids)
-            {
-                _humanoidsPositions.Add(humanoid,RandomPosition());
-            }
-        }
+            _pool = SceneState.Instance.Pool;
+
+            StartCoroutine(SpawnHumanoid());
+        } 
 
         private void FixedUpdate()
         {
-            foreach (var humanoid in _humanoidsPositions)
-            {
-                humanoid.Key.Control(AutoPilot, humanoid.Value.x, humanoid.Value.z);
-            }
+            RemoveDestinationReached();
         }
-        
-        private Vector3 RandomPosition() => new (Random.Range(-9, 9), 0, Random.Range(-9, 9));
+
+        private void RemoveDestinationReached()
+        {
+            _humanoidsPositions
+                .Where(humanoid =>
+                    Vector3.Distance(humanoid.Key.transform.position, humanoid.Value) < distanceThreshold)
+                .ToList()
+                .ForEach(humanoid =>
+                {
+                    _humanoidsPositions.Remove(humanoid.Key);
+                    _pool.Deposit(PoolType, humanoid.Key);
+                });
+        }
+
+        private IEnumerator SpawnHumanoid()
+        {
+            var human = _pool.Draw<Humanoid>(PoolType);
+            var targetPosition = RandomPosition();
+            
+            human.transform.position = RandomPosition();
+            _humanoidsPositions.Add(human, targetPosition);
+            
+            human.Control(AutoPilot, targetPosition.x, targetPosition.z);
+
+            yield return new WaitForSeconds(spawnRateSec);
+
+            StartCoroutine(SpawnHumanoid());
+        }
+
+        private static Vector3 RandomPosition() => new (Random.Range(-9, 9), 0, Random.Range(-9, 9));
     }
 }
--- Assets/Scripts/ObjPool/Pool.cs
+++ Assets/Scripts/ObjPool/Pool.cs
@@ -1,21 +1,19 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using Controllers.Controllable;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace ObjPool
 {
    public class Pool 
    {
-      private readonly Dictionary<PoolTypes, Queue<GameObject>> _poolDictionary;
+      private readonly Queue<Humanoid> _poolHumanoids;
 
       public Pool()
       {
-         _poolDictionary = new Dictionary<PoolTypes, Queue<GameObject>>();
-         
-         foreach (PoolTypes poolType in System.Enum.GetValues(typeof(PoolTypes)))
-         {
-            _poolDictionary.Add(poolType, new Queue<GameObject>());
-         }
+         _poolHumanoids = new Queue<Humanoid>();
       }
 
       public void Load(PoolLibrary library)
@@ -25,23 +23,64 @@ namespace ObjPool
             Enumerable.Range(0, library.Size).ToList().ForEach(_ =>
             {
                var instance = Object.Instantiate(prefab);
-               Deposit(library.Type, instance); 
+               EnqueueComponent(library.Type, instance); 
             });
          });
       }
-      
-      public void Deposit(PoolTypes poolType, GameObject gameObject)
+
+      private void EnqueueComponent(PoolTypes poolType, GameObject gameObject)
       {
          gameObject.SetActive(false);
-         _poolDictionary[poolType].Enqueue(gameObject);
-      } 
-      
-      public GameObject Draw(PoolTypes poolType)
-      {
-         var gameObject = _poolDictionary[poolType].Dequeue();
-         gameObject.SetActive(true);
          
-         return gameObject;
+         switch (poolType)
+         {
+            case PoolTypes.Humanoid:
+               var humanoid = gameObject.GetComponent<Humanoid>();
+
+               if (humanoid == null)
+               {
+                  throw new NullReferenceException("Humanoid component not found"); 
+               }
+               
+               _poolHumanoids.Enqueue(humanoid);
+               
+               break;
+            
+            default:
+               throw new ArgumentOutOfRangeException(nameof(poolType), poolType, null);
+         }
+      }
+
+      public void Deposit<T>(PoolTypes poolType, T component) where T : Component
+      {
+         component.gameObject.SetActive(false);
+         switch (poolType)
+         {
+            case PoolTypes.Humanoid:
+               _poolHumanoids.Enqueue(component as Humanoid);
+               break;
+            
+            default:
+               throw new ArgumentOutOfRangeException(nameof(poolType), poolType, null);
+         }
+      }
+      
+      public T Draw<T>(PoolTypes poolType) where T : Component
+      {
+         T component;
+         switch (poolType)
+         {
+            case PoolTypes.Humanoid:
+               component = _poolHumanoids.Dequeue() as T;
+               break;
+           
+            default:
+               throw new ArgumentOutOfRangeException(nameof(poolType), poolType, null);
+         }
+         
+         component.gameObject.SetActive(true);
+
+         return component;
       } 
    }
 }
