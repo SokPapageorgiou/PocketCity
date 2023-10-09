004ab75882e07f85c5b410bceeb5428258aa4a7b
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2023-10-09 10:22:38 +0200

    Add Location

---
+++ Assets/Scripts/Controllers/AI/InterestLocations/Location.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Commons;
+using ObjPool;
+using UnityEngine;
+
+namespace Controllers.AI.InterestLocations
+{
+    public class Location : MonoBehaviour
+    {
+        private Locations _locations;
+     
+        [SerializeField]
+        private List<AITypes> types;
+        
+        private void Awake()
+        {
+            _locations = SceneState.Instance.Locations;
+
+            types.ForEach(type => _locations.SetPoint(type, transform.position));
+        }
+    }
+}
