2045816e2fc6591fca80177880ab910f22366306
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2023-10-09 11:00:44 +0200

    Refactor General CleanUp

--- Assets/Scripts/Controllers/AI/InterestLocations/Location.cs
+++ Assets/Scripts/Controllers/AI/InterestLocations/Location.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using Commons;
-using ObjPool;
 using UnityEngine;
 
 namespace Controllers.AI.InterestLocations
--- Assets/Scripts/Controllers/AI/InterestLocations/Locations.cs
+++ Assets/Scripts/Controllers/AI/InterestLocations/Locations.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using ObjPool;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
